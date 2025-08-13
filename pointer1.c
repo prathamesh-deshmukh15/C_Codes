232c42ed97d8603b8f8b077aaedb7b0b09b50b3f
prathamesh-deshmukh15 <deshmukhprathamesh21@gmail.com>
2025-08-13 15:14:13 +0000

    13/08

---
+++ pointer1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=5,*p,*pp;
+    p=&a;
+    pp=&p;
+    printf("Value Of a is : %d\n",a);
+    printf("Address Of a is : %d\n",&a);
+    printf("Value Of a using pointer Is : %d\n",*p);
+    printf("Address Of a using pointer is : %d\n",p);
+    printf("Address Of Pointer p is : %d\n",pp);
+    return 0;
+}
