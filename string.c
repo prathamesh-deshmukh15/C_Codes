90fac5a8016177539f53f73ab3d431becb6f8f8b
prathamesh-deshmukh15 <deshmukhprathamesh21@gmail.com>
2025-08-20 15:25:49 +0000

    20/08

---
+++ string.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char ch[]="Hello World";
+    char ch1[]={'H','e','l','l','o',' ','W','o','r','l','d','\0'};
+    printf("Char Value Of Ch Is : %s\n",ch);
+    printf("Char Value Of Ch1 Is : %s\n",ch1);
+}
