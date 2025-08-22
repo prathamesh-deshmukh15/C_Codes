f900181f9eaa3bab0fe451dac3ac7cb314a97ab0
prathamesh-deshmukh15 <deshmukhprathamesh21@gmail.com>
2025-08-22 15:01:02 +0000

    22/08

--- math.c
+++ math.c
@@ -4,5 +4,15 @@ int main(){
     printf("%f\n",ceil(4.001));
     printf("%f\n",floor(4.001));
     printf("%f\n",sqrt(4));
-    printf("%d\n",abs(-4));
+    printf("%f\n",abs(-4));
+    printf("%f\n",fmod(3.54,1));
+    printf("%f\n",log(2.71));
+    printf("%f\n",log10(100));
+    printf("%f\n",fabs(-3.54));
+    printf("%f\n",pow(3,3));
+    printf("%f\n",exp(1));
+    printf("%f\n",round(3.54));
+    printf("%f\n",sin(0));
+    printf("%f\n",cos(0));
+    printf("%f\n",tan(0));
 }
---
+++ trigo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    double angle = 1;
+    double sin_val=sin(angle);
+    double cos_val=cos(angle);
+    double tan_val=tan(angle);
+
+    printf("For Angle %.2f\n",angle);
+    printf("Sine:%.2f\n",sin_val);
+    printf("Cosine:%.2f\n",cos_val);
+    printf("Tangent:%.2f\n",tan_val);
+   
+}
