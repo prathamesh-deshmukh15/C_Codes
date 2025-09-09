e9dd7d9c497ccd11a4fcbaf0da7cf9ee7b660777
prathamesh-deshmukh15 <deshmukhprathamesh21@gmail.com>
2025-09-09 14:52:41 +0000

    9/9

---
+++ even.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a ;
+    cout<<"Enter An Integer a : ";
+    cin>>a ;
+    if(a % 2==0){
+        cout<< a <<" Is An Even Number" << endl ;
+    }
+    else{
+        cout<< a <<" Is An Odd Number" << endl ;
+    }
+}
