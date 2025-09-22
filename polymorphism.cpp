3dd9fac3d810ff8245e3805fa7d5e2c072dd10f0
prathamesh-deshmukh15 <deshmukhprathamesh21@gmail.com>
2025-09-22 15:11:13 +0000

    22/09

---
+++ polymorphism.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=20;
+    string x="Prathamesh";
+    string y=" Deshmukh";
+    cout<<"The Value Of a+b is: "<<a+b<<endl;
+    cout<<"The Value Of x+y is: "<<x+y<<endl;
+    return 0;
+}
